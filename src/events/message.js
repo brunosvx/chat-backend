7a7b1102ba715f93574a877b399c01e94317a9a4
Bruno <mitozgamer137@gmail.com>
2022-02-21 17:44:04 -0300

    add last messages feature

--- src/events/message.js
+++ src/events/message.js
@@ -1,22 +1,26 @@
+import { validate as validateUuid } from 'uuid';
+
+export const lastMessages = [];
+
 
 export default function message(data, socket) {
-    console.log('func')
     
-    if(!validateMessage(data)) return;
-    console.log('validate');
+    const { id, name, text } = data;
 
-    const { name, text } = data;
+    if(!validateUuid(id) || !validateMessage(data)) return;
 
-    socket.broadcast.emit("message", { name: name.trim(), text: text.trim() });
+    socket.broadcast.emit("message", { name: name.trim(), text: text.trim(), id });
     
 }
 
 function validateMessage(data) {
     if(typeof data !== 'object') return false;
 
-    const { name, text } = data;
+    const { id: messageId, name, text } = data;
 
     if(typeof name !== 'string' || typeof text !== 'string') return false;
+
+    if(!lastMessages.every(({ id }) => id !== messageId)) return false;
 
     const nameLength = name.trim().length;
     const textLength = text.trim().length;
